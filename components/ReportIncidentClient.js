011f81a494f6fc74093619931e574113415bde59
stormymayday <stormymayday7@gmail.com>
2023-10-14 17:59:33 -0700

    Implemented leaflet and case fetching on MainClientPage. Implemented ReportIncidentClient.js page functionality

--- components/ReportIncidentClient.js
+++ components/ReportIncidentClient.js
@@ -1,81 +1,390 @@
+import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
+
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { auth, dataBase, storage } from "../services/firebase.js";
+import { signOut } from "firebase/auth";
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
 export default class ReportIncidentClient extends HTMLElement {
+
   constructor() {
+
     super();
+
+    this.user = JSON.parse(localStorage.getItem("user"));
+
+    // Used for image name
+    this.name = new Date().getTime();
+    this.downloadURL;
+    this.latitude;
+    this.longitude;
+
   }
+
+  async logOut() {
+
+    await signOut(auth);
+
+    Router.go(`/login`);
+
+  }
+
+  createCase = async (e) => {
+
+    // e.preventDefault();
+
+    // const user = JSON.parse(localStorage.getItem('user'));
+
+    const address = document.querySelector('#address').value;
+    const carMake = document.querySelector('#carMake').value;
+    const carModel = document.querySelector('#carModel').value;
+    const carType = document.querySelector('#carType').value;
+    const carColor = document.querySelector('#carColor').value;
+    const licensePlate = document.querySelector('#licensePlate').value;
+    const notes = document.querySelector('#notes').value;
+
+    // console.log(address);
+    // console.log(carMake);
+    // console.log(carType);
+    // console.log(carColor);
+    // console.log(licensePlate);
+    // console.log(notes);
+
+    try {
+
+      const response = await addDoc(collection(dataBase, "cases"), {
+
+        creationTime: serverTimestamp(),
+        completionTime: '',
+        reporterId: this.user.uid,
+        driverID: '',
+        coordinates: {
+          latitude: this.latitude,
+          longitude: this.longitude,
+        },
+        address: address,
+        image: this.downloadURL,
+        status: 'active',
+
+        carMake: carMake,
+        carModel: carModel,
+        carType: carType,
+        carColor: carColor,
+        licensePlate: licensePlate,
+
+        notes: notes,
+
+      });
+
+    } catch (error) {
+
+      console.error(error);
+
+    }
+
+  }
+
+  initializeMedia() {
+
+    // Custom Polyfills - Start
+    // Checking if there is no 'mediaDevices' in navigator
+    if (!('mediaDevices' in navigator)) {
+
+      console.log(`There are no Media Devices in the Navigator`);
+
+      // Creating mediaDevices object on navigator
+      navigator.mediaDevices = {};
+
+
+    }
+
+    // Checking if there is 'getUserMedia' property
+    if (!('getUserMedia' in navigator.mediaDevices)) {
+
+      console.log(`There is no 'getUserMedia' property`);
+
+      // Implementing our own getUserMedia function
+      navigator.mediaDevices.getUserMedia = function (constrains) {
+
+        // Binding Safari's OR Mozilla's implementation of getUserMedia 
+        let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+        // Checking if getUserMedia is undefined
+        if (!getUserMedia) {
+          return Promise.reject(new Error('getUserMedia is not implemented!'));
+        }
+
+        return new Promise(function (resolve, reject) {
+
+          getUserMedia.call(navigator, constrains, resolve, reject);
+
+        });
+
+      }
+
+    }
+    // Custom Polyfills - End
+
+    // Getting access to the device camera
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        width: {
+          min: 1280,
+          ideal: 1920,
+          max: 2560,
+        },
+        height: {
+          min: 720,
+          ideal: 1080,
+          max: 1440
+        },
+        facingMode: 'environment'
+      }
+    }).then((stream) => {
+
+      // Access granted
+      // Outputting the stream
+      document.querySelector('#player').srcObject = stream;
+      document.querySelector('#player').style.display = 'block';
+
+    }).catch(function (error) {
+
+      // Access denied OR there is no media access
+      // Displaying image picker
+      document.querySelector('#pick-image').style.display = 'block';
+
+    });
+
+  }
+  // End of initializeMedia method
+
+  stopMedia() {
+
+    let videoPlayer = this.querySelector('#player');
+
+    // Stopping the video stream
+    videoPlayer.srcObject.getVideoTracks().forEach((track) => {
+
+      track.stop();
+
+    });
+
+  }
+  // End of stopMedia method
+
+  // This method converts a base64 url into a BLOB
+  base64toBLOB(dataURI) {
+
+    let byteString = atob(dataURI.split(',')[1]);
+
+    let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    let ab = new ArrayBuffer(byteString.length);
+
+    let ia = new Uint8Array(ab);
+
+    for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    let blob = new Blob([ab], { type: mimeString });
+
+    return blob;
+  }
+  // End of dataURItoBlob method
 
   connectedCallback() {
-    const template = document.getElementById("report-incident-client");
+
+    // console.log(name);
+
+    // Getting template from the DOM
+    const template = document.getElementById('report-incident-client');
+
+    // Cloning the template
     const content = template.content.cloneNode(true);
+
+    // Appending content to the DOM
     this.appendChild(content);
 
-    const fileInput = this.querySelector("#fileInput");
-    const uploadedImage = this.querySelector("#uploadedImage");
-    const uploadButton = this.querySelector("#uploadButton");
-    const uploadMoreButton = this.querySelector("#uploadMoreButton");
-    const captureButton = this.querySelector("#captureButton");
+    const user = JSON.parse(localStorage.getItem('user'));
 
-    let stream; // Store the stream globally
+    if (user) {
 
-    captureButton.addEventListener("click", function () {
-      const constraints = { video: true };
+      this.querySelector('h1').innerHTML = `Welcome ${user.email}`;
 
-      navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function (s) {
-          stream = s; // Store the stream
-          const video = document.createElement("video");
-          uploadedImage.parentElement.appendChild(video);
+      // Testing if navigator.geolocation is supported by the browser
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
 
-          video.srcObject = stream;
-          video.play();
+          // Succuss Callback Code:
 
-          video.addEventListener("click", function () {
-            const canvas = document.createElement("canvas");
-            const context = canvas.getContext("2d");
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
+          // Destructuring latitude and longitude from position.coords object
+          const { latitude } = position.coords;
+          const { longitude } = position.coords;
+          const coordinates = [latitude, longitude];
 
-            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+          // Leaflet Code - Start
+          // Rendering map centered on a current user location (coordinates) with max zoom-in setting
+          const map = L.map('map').setView(coordinates, 18);
 
-            const dataURL = canvas.toDataURL("image/png");
-            uploadedImage.src = dataURL;
-            uploadButton.style.display = "none";
-            uploadMoreButton.style.display = "block";
+          // Tilelayer
+          L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          }).addTo(map);
 
-            // Upload img to firebase so we can pass to leaflet
+          // Displaying a Marker with current user coordinates
+          L.marker(coordinates).addTo(map)
+            .bindPopup(
+              L.popup({
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+              })
+            )
+            .setPopupContent('You are currently here')
+            .openPopup();
 
-            // Turn off the camera
-            stream.getTracks().forEach((track) => track.stop());
+          // Variable for tracking user clicks on the map
+          let clickMarker = {};
+
+          // Adding 'click' eventListener to the map 
+          map.on('click', (mapEvent) => {
+
+            // Destructuring latitude and longitude from mapEvent.latlng object
+            const { lat, lng } = mapEvent.latlng;
+
+            this.latitude = lat;
+            this.longitude = lng;
+
+            // Checking if clickMarker already on the map
+            if (clickMarker != undefined) {
+
+              // Removing clickMarker from the map
+              map.removeLayer(clickMarker);
+
+            }
+
+            //Adding clickMarker to the map
+            clickMarker = L.marker([lat, lng]).addTo(map)
+              .bindPopup(
+                L.popup({
+                  autoClose: false,
+                  closeOnClick: false,
+                  className: 'running-popup',
+                })
+              )
+              .setPopupContent('Incident location')
+              .openPopup();
+
+            console.log(`User clicked on ${lat} ${lng} coordinates`);
+
           });
-        })
-        .catch(function (error) {
-          console.error("Error accessing the camera:", error);
+
+          // Leaflet Code - End
+
+        }, () => {
+
+          // Error Callback Code:
+
+          alert(`Unfortunately, TowTackle was not able to pick up your position.`);
+
         });
-    });
 
-    // DISPLAY IMGS ( HAVE to CREATE A GALERY To DISPLAY IMGS)
-
-    fileInput.addEventListener("change", function () {
-      const file = this.files[0];
-      const reader = new FileReader();
-
-      reader.onload = function (e) {
-        uploadedImage.src = e.target.result;
-        uploadButton.style.display = "none";
-        uploadMoreButton.style.display = "block";
-
-        // Upload img to firebase so we can pass to leaflet
-      };
-      reader.readAsDataURL(file);
-    });
-
-    this.querySelector("#btn-click-go-to-incident-info").addEventListener(
-      "click",
-      () => {
-        app.router.go("/incident-info");
       }
-    );
+      // end of navigator / Leaflet
+
+      // Image Capture - Start
+      let videoPlayer = this.querySelector('#player');
+      let canvasElement = this.querySelector('#canvas');
+      let captureBtn = this.querySelector('#capture-btn');
+      let imagePicker = this.querySelector('#image-picker');
+      let imagePickerDiv = this.querySelector('#pick-image');
+      let picture;
+
+      this.initializeMedia();
+
+      captureBtn.addEventListener('click', (event) => {
+
+        // Showing the canvas
+        canvasElement.style.display = 'block';
+
+        // Hiding the video player
+        videoPlayer.style.display = 'none';
+
+        // Hiding the capture button
+        captureBtn.style.display = 'none';
+
+        // Creating context for the canvas
+        const context = canvasElement.getContext('2d');
+
+        // Drawing image on the canvas
+        context.drawImage(videoPlayer, 0, 0, canvas.width, videoPlayer.videoHeight / (videoPlayer.videoWidth / canvas.width));
+
+        // Stopping the video stream
+        videoPlayer.srcObject.getVideoTracks().forEach((track) => {
+
+          track.stop();
+
+        });
+
+        // Converting canvasElement into a BLOB
+        picture = this.base64toBLOB(canvasElement.toDataURL());
+
+        console.log(picture);
+
+        const storageRef = ref(storage, `${this.name}`);
+
+        // 'file' comes from the Blob or File API
+        uploadBytes(storageRef, picture).then((snapshot) => {
+
+          getDownloadURL(snapshot.ref).then((downloadURL) => {
+
+            this.downloadURL = downloadURL;
+
+            console.log('File available at', downloadURL);
+          });
+
+        });
+
+      });
+      // Image Capture - End
+
+    }
+
+    this.querySelector("#back-btn").addEventListener("click", async (event) => {
+
+      // Stopping the video stream
+      this.stopMedia();
+
+      app.router.go(`/main-page`);
+
+    });
+
+    this.querySelector("#incident-form").addEventListener("submit", async (event) => {
+
+      event.preventDefault();
+
+      // Stopping the video stream
+      this.stopMedia();
+
+      this.createCase();
+
+    });
+
+    this.querySelector("#logout-btn").addEventListener("click", async (event) => {
+
+      // Stopping the video stream
+      this.stopMedia();
+
+      localStorage.clear();
+
+      await this.logOut();
+
+    });
+
   }
+
 }
 
 customElements.define("report-incident-client", ReportIncidentClient);
--- services/firebase.js
+++ services/firebase.js
@@ -6,9 +6,10 @@ import {
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-
+import { getStorage } from "firebase/storage";
 import { firebaseConfig } from "./config.js";
 
 export const appPath = initializeApp(firebaseConfig);
 export const auth = getAuth(appPath);
 export const dataBase = getFirestore(appPath);
+export const storage = getStorage(appPath);
