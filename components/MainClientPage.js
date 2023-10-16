b3941804852a419304514af93e799305f01c0307
stormymayday <stormymayday7@gmail.com>
2023-10-15 17:24:25 -0700

    Added edit case functionality on the user side

--- components/MainClientPage.js
+++ components/MainClientPage.js
@@ -1,8 +1,9 @@
 import 'leaflet/dist/leaflet.css';
 import L from "leaflet";
+import Router from '../services/Router.js';
 import { signOut } from "firebase/auth";
-import { auth, dataBase } from "../services/firebase.js"
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { auth, dataBase } from "../services/firebase.js";
+import { collection, query, where, getDocs, doc, updateDoc } from "firebase/firestore";
 
 export default class MainClientPage extends HTMLElement {
 
@@ -13,6 +14,8 @@ export default class MainClientPage extends HTMLElement {
     this.user = JSON.parse(localStorage.getItem("user"));
 
     this.map;
+
+    this.activeCases = null;
 
   }
 
@@ -26,7 +29,7 @@ export default class MainClientPage extends HTMLElement {
 
   displayMap = async () => {
 
-    navigator.geolocation.getCurrentPosition((position) => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
 
       // Succuss Callback Code:
 
@@ -58,7 +61,133 @@ export default class MainClientPage extends HTMLElement {
 
       try {
 
-        this.getActiveCases();
+        await this.getActiveCases();
+
+        this.activeCases.forEach((incidentCase) => {
+
+          console.log(incidentCase.id, " => ", incidentCase.data());
+
+          const { latitude, longitude } = incidentCase.data().coordinates;
+
+          const coordinates = [latitude, longitude];
+
+          let incidentMarker = {};
+
+          incidentMarker = L.marker([latitude, longitude], { draggable: 'true' }).addTo(this.map)
+            .bindPopup(
+              L.popup({
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+              })
+            )
+            // .setPopupContent(incidentCase.data().notes)
+            .setPopupContent(`
+              <div id="popup-form">
+                <form id="my-form">
+
+                  <label for="address">Address:</label>
+                  <input type="text" id="address" name="address" value="${incidentCase.data().address}" /><br />
+
+                  <label for="carMake">Car Make:</label>
+                  <input type="text" id="carMake" name="carMake" value="${incidentCase.data().carMake}" /><br />
+
+                  <label for="carModel">Car Model:</label>
+                  <input type="text" id="carModel" name="carModel" value="${incidentCase.data().carModel}" /><br />
+
+                  <label for="carType">Car Type:</label>
+                  <select id="carType" name="carType">
+                    <option value="SUV">SUV</option>
+                    <option value="Sedan">Sedan</option>
+                    <option value="Hatchback">Hatchback</option>
+                    <option value="Convertible">Convertible</option>
+                    <option value="Truck">Truck</option>
+                  </select><br /><br />
+
+                  <label for="carColor">Car Color:</label>
+                  <input type="text" id="carColor" name="carColor" value="${incidentCase.data().carColor}" /><br />
+
+                  <label for="licensePlate">License Plate:</label>
+                  <input type="text" id="licensePlate" name="licensePlate" value="${incidentCase.data().licensePlate}" /><br />
+
+                  <label for="notes">Notes:</label>
+                  <textarea name="notes" id="notes" cols="30" rows="10">${incidentCase.data().notes}</textarea>
+                  <button id="edit-btn">Edit</button>
+                  <button id="cancel-btn">Cancel</button>
+                </form>
+              </div>
+            `);
+          // .openPopup();
+
+          // Adding incident case id and incident data to the marker
+          incidentMarker.customData = { id: incidentCase.id, ...incidentCase.data() };
+
+          // Drag Marker functionality
+          incidentMarker.on('dragend', function (event) {
+            const newLatLng = event.target.getLatLng();
+            const newLatitude = newLatLng.lat;
+            const newLongitude = newLatLng.lng;
+
+            // Update your data or perform actions with the new coordinates
+            console.log('New coordinates:', newLatitude, newLongitude);
+          });
+
+
+          incidentMarker.on('click', function () {
+
+            // Adding click event listener to the Marker
+            document.querySelector(`#edit-btn`).addEventListener('click', async (event) => {
+
+              event.preventDefault();
+
+              console.log(`You clicked Edit button`);
+
+              // Getting input values
+              const address = document.querySelector('#address').value;
+              const carMake = document.querySelector('#carMake').value;
+              const carModel = document.querySelector('#carModel').value;
+              const carType = document.querySelector('#carType').value;
+              const carColor = document.querySelector('#carColor').value;
+              const licensePlate = document.querySelector('#licensePlate').value;
+              const notes = document.querySelector('#notes').value;
+
+              // Editing the Case
+              const caseRef = doc(dataBase, "cases", incidentCase.id);
+              await updateDoc(caseRef, {
+                address: address,
+                carMake: carMake,
+                carModel: carModel,
+                carType: carType,
+                carColor: carColor,
+                licensePlate: licensePlate,
+                notes, notes,
+              });
+
+            });
+
+            // Adding click event listener to the 'Cancel' button
+            document.querySelector(`#cancel-btn`).addEventListener('click', async (event) => {
+
+              event.preventDefault();
+
+              console.log(`You clicked Cancel button`);
+
+              // Setting status to 'cancelled'
+              const caseRef = doc(dataBase, "cases", incidentCase.id);
+              await updateDoc(caseRef, {
+                address: 'cancelled',
+
+              });
+
+              Router.go('/main-page');
+
+            });
+
+            console.log(this);
+
+          });
+
+        });
 
       } catch (error) {
 
@@ -82,31 +211,13 @@ export default class MainClientPage extends HTMLElement {
     const q = query(collection(dataBase, "cases"), where("status", "==", 'active'),
       where("reporterId", "==", this.user.uid));
 
+    console.log(this.user.uid);
+
     const querySnapshot = await getDocs(q);
 
-    querySnapshot.forEach((doc) => {
+    this.activeCases = querySnapshot;
 
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-
-      const { latitude, longitude } = doc.data().coordinates;
-
-      const coordinates = [latitude, longitude];
-
-      let incidentMarker = {};
-
-      incidentMarker = L.marker([latitude, longitude]).addTo(this.map)
-        .bindPopup(
-          L.popup({
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-        )
-        .setPopupContent(doc.data().notes)
-        .openPopup();
-
-    });
+    // console.log(this.activeCases);
 
   }
 
@@ -155,7 +266,6 @@ export default class MainClientPage extends HTMLElement {
 
       }
     );
-
 
   }
 }
