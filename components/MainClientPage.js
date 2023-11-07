50374d52b78f3cae6a0763f6f5182d1adcc22825
stormymayday <stormymayday7@gmail.com>
2023-11-06 21:09:32 -0800

    Switched back to original tile

--- components/MainClientPage.js
+++ components/MainClientPage.js
@@ -53,8 +53,11 @@ export default class MainClientPage extends HTMLElement {
 				const originalTile =
 					'https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
 
+				// Mapbox Monochrome
+				const monochrome = `https://api.mapbox.com/styles/v1/stormymayday/${import.meta.env.VITE_MAPBOX_STYLE}/tiles/256/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}`;
+
 				// Tilelayer
-				L.tileLayer(`https://api.mapbox.com/styles/v1/stormymayday/${import.meta.env.VITE_MAPBOX_STYLE}/tiles/256/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}`, {
+				L.tileLayer(originalTile, {
 					attribution:
 						'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 				}).addTo(this.map);
--- components/ReportIncidentClient.js
+++ components/ReportIncidentClient.js
@@ -144,8 +144,11 @@ export default class ReportIncidentClient extends HTMLElement {
 						// Original Tile
 						const originalTile = 'https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
 
+						// Mapbox Monochrome
+						const monochrome = `https://api.mapbox.com/styles/v1/stormymayday/${import.meta.env.VITE_MAPBOX_STYLE}/tiles/256/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}`;
+
 						// Tilelayer
-						L.tileLayer(`https://api.mapbox.com/styles/v1/stormymayday/${import.meta.env.VITE_MAPBOX_STYLE}/tiles/256/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}`, {
+						L.tileLayer(originalTile, {
 							attribution:
 								'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 						}).addTo(map);
