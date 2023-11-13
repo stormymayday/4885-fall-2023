a79bd9a349363bf2e17ccf46d84f392af3ec4ccb
stormymayday <stormymayday7@gmail.com>
2023-11-12 21:38:10 -0800

    Changed tile layer to monochrome

--- components/DriverViewCasePage.js
+++ components/DriverViewCasePage.js
@@ -98,7 +98,7 @@ export default class DriverViewCasePage extends HTMLElement {
         const monochrome = `https://api.mapbox.com/styles/v1/stormymayday/${import.meta.env.VITE_MAPBOX_STYLE}/tiles/256/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}`;
 
         // Tilelayer
-        L.tileLayer(originalTile, {
+        L.tileLayer(monochrome, {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this.map);
 
